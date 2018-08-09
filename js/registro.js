a1dd831ead571835aef71f1520d5a454f360a08a
DiegoNaranjo913 <30935581+DiegoNaranjo913@users.noreply.github.com>
2018-08-08 23:51:44 -0500

    cambios
    se ha arreglado la forma de recibir los datos al registrarse

--- js/registro.js
+++ js/registro.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
-   
-   $("#cancelar").on('click',function(){
-       location = 'index.html';
-   });
+
+    $("#cancelar").on('click', function () {
+        location = 'index.html';
+    });
+
+    $("#Registrar").on('click', function () {
+        var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
+
+        if ($("#clave").val() === "" || $("#claveC").val() === "" || $("#genero option:selected").text() === "Seleccione" || $("#correo").val() === "" || $("#nombre").val() === "") {
+            swal("Alguno de los Campos Estan Vacios o no ha Seleccionado el Genero");
+
+        } else if ($("#clave").val() !== $("#claveC").val()) {
+            swal("La Contrasenia y su Cofirmacion son Diferentes!");
+        } else if (!regex.test($('#correo').val().trim())) {
+            swal("La direccion e-mail parece incorrecta");
+        } else {
+            swal("Se ha Registrado Correctamente");
+            
+        }
+    });
 });
 
 
