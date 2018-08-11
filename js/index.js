0670675092c74459c122afb92ab6cd81f375d026
DiegoNaranjo913 <30935581+DiegoNaranjo913@users.noreply.github.com>
2018-08-10 23:49:34 -0500

    arreglos finales
    se agregaron los ajax para el envio de datos y se le agrego un texto a la pagina despues del ingreso

--- js/index.js
+++ js/index.js
@@ -1,9 +1,112 @@
+var config = {
+        apiKey: "AIzaSyAkDlGqJE3SPMkrF_ZbbRX4V-e_YoBTmuU",
+        authDomain: "bicired-3ca7d.firebaseapp.com",
+        databaseURL: "https://bicired-3ca7d.firebaseio.com",
+        projectId: "bicired-3ca7d",
+        storageBucket: "bicired-3ca7d.appspot.com",
+        messagingSenderId: "899099425162"
+    };
+    firebase.initializeApp(config);
 $(document).ready(function () {
-   $("#dialog").dialog({autoOpen: false,title:"Puedes iniciar con tus redes sociales",width:350,resizable: false});
-   $("#botonRedes").on('click',function(){
-       $("#dialog").dialog('open');
-   });
-   $("#registrar").on('click',function(){
-       location = 'registro.html';
-   });
+    $("#dialog").dialog({autoOpen: false, title: "Puedes iniciar con tus redes sociales", width: 350, resizable: false});
+    $("#Ingresar").on('click', function () {
+        console.log("click");
+        if ($("#usuario").val() === "" || $("#clave").val() === "") {
+            swal("Mira Bien los Campos", "Alguno de los Campos Esta Vacio o no ha Seleccionado el Genero", "error");
+        } else {
+            swal({
+                title: "Confirmado",
+                text: "Se ha Registrado Correctamente",
+                type: "success",
+                confirmButtonClass: "btn-danger",
+                confirmButtonText: "Conformado",
+                closeOnConfirm: false
+            }, function () {
+                window.location.href = 'paginaPrincipal.html';
+            });
+        }
+    });
+    $("#botonRedes").on('click', function () {
+        $("#dialog").dialog('open');
+    });
+    $("#registrar").on('click', function () {
+        location = 'registro.html';
+    });
+
+    $("#btngoogle").on('click', function () {
+        google();
+        
+    });
+    
 });
+
+var google = function () {
+    console.log("Entroooo");
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/userinfo.email');
+    firebase.auth().languageCode = 'pt';
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        console.log("Result ", result["user"]["email"]);
+        var emailgoogle = result["user"]["email"];
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        
+        ajaxgoogle(emailgoogle);
+        // ...
+    }).catch(function (error) {
+        console.error("Result ", error);
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+};
+
+var facebook = function () {
+    console.log("Entroooo");
+    var provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('email');
+    firebase.auth().languageCode = 'es_ES';
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        console.log("Result ", result);
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        console.error(error);
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+};
+
+function ajaxgoogle(email){
+    var G="G";
+    var parametros ={emailgoogle:email,constante:G};
+    
+    console.log(parametros);
+    
+    $.ajax({
+       data: parametros,
+       type: 'POST',
+       url:'bicired_backend/usuario/'
+//        success: function (data) {
+//            swal(data);
+//        }
+       
+    });
+    
+}
--- js/registro.js
+++ js/registro.js
@@ -8,17 +8,40 @@ $(document).ready(function () {
         var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
 
         if ($("#clave").val() === "" || $("#claveC").val() === "" || $("#genero option:selected").text() === "Seleccione" || $("#correo").val() === "" || $("#nombre").val() === "") {
-            swal("Alguno de los Campos Estan Vacios o no ha Seleccionado el Genero");
+            swal("Mira Bien los Campos", "Alguno de los Campos Esta Vacio o no ha Seleccionado el Genero", "error");
 
         } else if ($("#clave").val() !== $("#claveC").val()) {
-            swal("La Contrasenia y su Cofirmacion son Diferentes!");
+            swal("Algo Esta Mal", "La Contrasenia y su Cofirmacion son Diferentes", "error");
+            console.log("Password" + " " + $("#clave").val());
+            console.log("Confirmacion Password" + " " + $("#claveC").val());
         } else if (!regex.test($('#correo').val().trim())) {
-            swal("La direccion e-mail parece incorrecta");
+            swal("Algo Esta Mal", "La direccion e-mail parece incorrecta", "error");
         } else {
-            swal("Se ha Registrado Correctamente");
-            
+
+            swal({
+                title: "Confirmado",
+                text: "Se ha Registrado Correctamente",
+                type: "success",
+                confirmButtonClass: "btn-danger",
+                confirmButtonText: "Conformado",
+                closeOnConfirm: false
+            }, function () {
+                ajaxregistro($("#nombre").val(), $("#correo").val(), $("#genero option:selected").text(), $("#clave").val(), $("#claveC").val());
+                window.location.href = 'index.html';
+            });
         }
     });
 });
 
-
+function ajaxregistro(nombre, correo, genero, clave, claveConfirmada) {
+    var parametros = {nombre: nombre, correo: correo, genero: genero, clave: clave, claveConfirmada: claveConfirmada};
+    $.ajax({
+        data: parametros,
+        type: 'POST',
+        url: 'bicired_backend/usuario/'
+//        success: function (data) {
+//            swal(data);
+//        }
+    });
+}
+;
