d5bc6aebd73e0c55f902630e19a2dca5ad511122
Aric Dayan Gutierrez Sanchez <adgsgutierrez@gmail.com>
2018-09-30 10:39:28 -0500

    Ajuste a la ventana de mensajes

--- js/crearMensaje.js
+++ js/crearMensaje.js
@@ -14,10 +14,11 @@ $(document).ready(function () {
         url: URL_USUARIO,
         success: function (data) {
             data = JSON.parse(data);
+            console.log(data);
             $.each(data.datos, function (i, o) {
                 if (o["foto"] === null) {
                     if (o["genero"] === "F") {
-                        caja = "<div class='cajaper' style='width:100%;height:80px;border-bottom: 2px solid darkgray;cursor:pointer' data-correo='" + o["correo"] + "'><div class='row' style='padding-top: 14px;'><div class='col-sm-4' style='text-align:center'><img style='width:50px;height:50px;border-radius:25px;' src='img/perfil-mujer.jpg'/></div><div class='col-sm-8' style='text-transform: capitalize;font-size: 20px;cursor:pointer'><label style='cursor:pointer'>" + o["nombre"] + "</label></div></div></div>";
+                        caja = "<div class='cajaper' class='item-send-chat' data-correo='" + o["correo"] + "'><div class='row' style='padding-top: 14px;'><div class='col-sm-4' style='text-align:center'><img style='width:50px;height:50px;border-radius:25px;' src='img/perfil-mujer.jpg'/></div><div class='col-sm-8' style='text-transform: capitalize;font-size: 20px;cursor:pointer'><label style='cursor:pointer'>" + o["nombre"] + "</label></div></div></div>";
                     } else {
                         caja = "<div class='cajaper' style='width:100%;height:80px;border-bottom: 2px solid darkgray;cursor:pointer' data-correo='" + o["correo"] + "'><div class='row' style='padding-top: 14px;'><div class='col-sm-4' style='text-align:center;'><img style='width:50px;height:50px;border-radius:25px;' src='img/perfil-hombre.jpg'/></div><div class='col-sm-8' style='text-transform: capitalize;font-size: 20px;cursor:pointer'><label style='cursor:pointer'>" + o["nombre"] + "</label></div></div></div>";
                     }
@@ -62,9 +63,9 @@ function listamensajes() {
             data = JSON.parse(data);
             $.each(data.datos, function (i, o) {
                 if (o["envia"] === correo) {
-                    caja2 = "<div class='cajaper' style='width:100%;height:80px;border-bottom: 2px solid darkgray;text-align:right'><label>" + o["mensaje"] + "</label></div>";
+                    caja2 = "<div><div class='cajaper item-send-chat'><label>" + o["mensaje"] + "</label></div></div>";
                 } else {
-                    caja2 = "<div class='cajaper' style='width:100%;height:80px;border-bottom: 2px solid darkgray;text-align:left'><label>" + o["mensaje"] + "</label></div>";
+                    caja2 = "<div><div class='cajaper item-recevied-chat'><label>" + o["mensaje"] + "</label></div></div>";
                 }
                 $("#cajamen").append(caja2);
             });
@@ -72,4 +73,3 @@ function listamensajes() {
     });
     $("#textarea").val("");
 }
-
