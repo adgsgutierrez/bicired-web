cda3d9229197a7af5c4d03115985b7906d22ae96
Aric Dayan Gutierrez Sanchez <adgsgutierrez@gmail.com>
2018-09-30 11:51:21 -0500

    Ajuste a la vista de mensajes con su mejora a la vista

--- js/crearMensaje.js
+++ js/crearMensaje.js
@@ -18,12 +18,12 @@ $(document).ready(function () {
             $.each(data.datos, function (i, o) {
                 if (o["foto"] === null) {
                     if (o["genero"] === "F") {
-                        caja = "<div class='cajaper' class='item-send-chat' data-correo='" + o["correo"] + "'><div class='row' style='padding-top: 14px;'><div class='col-sm-4' style='text-align:center'><img style='width:50px;height:50px;border-radius:25px;' src='img/perfil-mujer.jpg'/></div><div class='col-sm-8' style='text-transform: capitalize;font-size: 20px;cursor:pointer'><label style='cursor:pointer'>" + o["nombre"] + "</label></div></div></div>";
+                        caja = "<div class='cajaper' data-correo='" + o["correo"] + "'><div class='row' style='padding-top: 14px;'><div class='col-sm-4' style='text-align:center'><img style='width:50px;height:50px;border-radius:25px;' src='img/perfil-mujer.jpg'/></div><div class='col-sm-8' style='text-transform: capitalize;font-size: 20px;cursor:pointer'><label style='cursor:pointer'>" + o["nombre"] + "</label></div></div></div>";
                     } else {
-                        caja = "<div class='cajaper' style='width:100%;height:80px;border-bottom: 2px solid darkgray;cursor:pointer' data-correo='" + o["correo"] + "'><div class='row' style='padding-top: 14px;'><div class='col-sm-4' style='text-align:center;'><img style='width:50px;height:50px;border-radius:25px;' src='img/perfil-hombre.jpg'/></div><div class='col-sm-8' style='text-transform: capitalize;font-size: 20px;cursor:pointer'><label style='cursor:pointer'>" + o["nombre"] + "</label></div></div></div>";
+                        caja = "<div class='cajaper' data-correo='" + o["correo"] + "'><div class='row' style='padding-top: 14px;'><div class='col-sm-4' style='text-align:center;'><img style='width:50px;height:50px;border-radius:25px;' src='img/perfil-hombre.jpg'/></div><div class='col-sm-8' style='text-transform: capitalize;font-size: 20px;cursor:pointer'><label style='cursor:pointer'>" + o["nombre"] + "</label></div></div></div>";
                     }
                 } else {
-                    caja = "<div class='cajaper' style='width:100%;height:80px;border-bottom: 2px solid darkgray;cursor:pointer' data-correo='" + o["correo"] + "'><div class='row' style='padding-top: 14px;'><div class='col-sm-4' style='text-align:center'><img style='width:50px;height:50px;border-radius:25px;' src='" + o["foto"] + "'/></div><div class='col-sm-8' style='text-transform: capitalize;font-size: 20px;cursor:pointer'><label style='cursor:pointer'>" + o["nombre"] + "</label></div></div></div>";
+                    caja = "<div class='cajaper' style='width:100%;height:80px;cursor:pointer' data-correo='" + o["correo"] + "'><div class='row' style='padding-top: 14px;'><div class='col-sm-4' style='text-align:center'><img style='width:50px;height:50px;border-radius:25px;' src='" + o["foto"] + "'/></div><div class='col-sm-8' style='text-transform: capitalize;font-size: 20px;cursor:pointer'><label style='cursor:pointer'>" + o["nombre"] + "</label></div></div></div>";
                 }
 
                 $("#cajaamigos").data("correo", o["correo"]);
@@ -63,9 +63,9 @@ function listamensajes() {
             data = JSON.parse(data);
             $.each(data.datos, function (i, o) {
                 if (o["envia"] === correo) {
-                    caja2 = "<div><div class='cajaper item-send-chat'><label>" + o["mensaje"] + "</label></div></div>";
+                    caja2 = "<div><div class='cajaper item-send-chat'><span>" + o["mensaje"] + "</span></div></div>";
                 } else {
-                    caja2 = "<div><div class='cajaper item-recevied-chat'><label>" + o["mensaje"] + "</label></div></div>";
+                    caja2 = "<div><div class='cajaper item-recevied-chat'><span>" + o["mensaje"] + "</span></div></div>";
                 }
                 $("#cajamen").append(caja2);
             });
